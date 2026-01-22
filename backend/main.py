9a0a4d031cdf205cbe8b566ccf4ee8abb188aa1d
Egor <bananaxvii812@gmail.com>
2026-01-22 20:43:32 +0500

    upd. requirements.txt and write note in main.py

---
+++ backend/main.py
@@ -0,0 +1,9 @@
+# API which can help you pick need hero.
+# 
+# Usage:
+#       1. Fetch live meta-data from the game's official endpoint;
+#       2. Filter heroes based on user's preferred playstyle and skill level.
+#       3. Return the top 3 recommendations with brief strategy quides.
+#
+# Note:
+#      It's simple project without frontend part (only BACKEND).
